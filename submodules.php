8860707106216722aebe80d0dab8b70c1b3931bd
1437 <u41108988@false.(none)>
2012-05-06 14:10:15 -0400

    Update: Website based changes

--- submodules.php
+++ submodules.php
@@ -1,7 +1,9 @@
 <?php
   'git add -A';
   'git commit -m "Server: Website based changes';
+  echo "Commited server side changes";
   'git pull origin master';
+  echo "pulling for each submodule";
   'git submodule foreach git pull origin master';
   'git add -A';
   'git commit -m "Server: Updated submodules to latest';
