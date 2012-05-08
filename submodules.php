0f0296c8863874917c11eec1bbfd5091be8dbc49
William Weiskopf <worldwidewilly@comcast.net>
2012-05-07 21:02:46 -0600

    I think I discovered why my submodules file wasn't working... I was using the wrong kind of tick mark.

--- submodules.php
+++ submodules.php
@@ -1,12 +1,9 @@
 <?php
-  'cd /kunden/homepages/13/d176034565/htdocs/weiskopf/william';
-  'git add -A';
-  'git commit -m "Server: Website based changes';
-  echo "Commited server side changes";
-  'git pull origin master';
-  echo "pulling for each submodule";
-  'git submodule foreach git pull origin master';
-  'git add -A';
-  'git commit -m "Server: Updated submodules to latest';
-  'git push origin master';
+  `git add -A`;
+  `git commit -m "Server: Website based changes`;
+  `git pull origin master`;
+  `git submodule foreach git pull origin master`;
+  `git add -A`;
+  `git commit -m "Server: Updated submodules to latest`;
+  `git push origin master`;
 ?>
